f8513564d41a40b96ed5562206f281487b47048e
Ayan <ayan2572006@gmail.com>
2024-04-03 12:34:20 +0530

    protected route done

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,28 @@ import Shop from "./pages/Shop/Shop";
 import ProductPage from "./pages/Shop/ProductPage";
 import { useDispatch } from "react-redux";
 import { useFetchUserProfileQuery } from "./redux/api/userApi";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 
 const App = () => {
   const dispatch = useDispatch()
   const {data}=useFetchUserProfileQuery()
- console.log(data)
+
   return (
     <div className="min-h-screen w-screen">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route element={<ProtectedRoute isAuthenticated={data ? false : true}/>}>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          </Route>
+         
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop/product/:id" element={<ProductPage />} />
+
         </Routes>
         <Footer />
         <Toaster position="bottom-center" />
