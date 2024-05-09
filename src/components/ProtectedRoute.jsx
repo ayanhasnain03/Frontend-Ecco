49acc04138df6b20305e907e50f9c0d616dde7fc
Ayan <ayan2572006@gmail.com>
2024-05-09 14:00:18 +0530

    fix

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,8 +3,8 @@ import { Navigate, Outlet } from "react-router-dom";
 
 
 const ProtectedRoute = ({isAuthenticated, children, adminRoute, isAdmin, redirect="/"}) => {
-  if (!isAuthenticated) return <Navigate to={"/login"} />;
-  if (adminRoute && !isAdmin) return <Navigate to={redirect} />;
+  if (!isAuthenticated) return <Navigate to={redirect} />;
+  if (adminRoute && !isAdmin) return <Navigate to={"/login"} />;
   return children ? children : <Outlet />;
 }
 
